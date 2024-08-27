fd71a8ef27887a8b2a95b278784c93d77835911a
ranwei <r13228115513@163.com>
2024-08-27 17:33:02 +0800

    添加拦截器

---
+++ src/main/java/com/example/wiki/interceptor/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.example.wiki.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/v1/user/login","/v1/user/register");
+    }
+}
---
+++ src/main/java/com/example/wiki/interceptor/LoginInterceptor.java
@@ -0,0 +1,61 @@
+package com.example.wiki.interceptor;
+
+import com.example.wiki.dto.ResultVO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getParameter("token");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<"+ token);
+        if (token == null) {
+            ResultVO resultVO = new ResultVO(401, "请先登录", null);
+            doResponse(response, resultVO);
+            return false;
+        }else {
+            try {
+                JwtParser jwtParser = Jwts.parser();
+                jwtParser.setSigningKey("secret");
+                jwtParser.parseClaimsJws(token);
+                return true;
+            }catch (ExpiredJwtException e) {
+                ResultVO resultVO = new ResultVO(401, "token过期", null);
+                doResponse(response, resultVO);
+                return false;
+            }catch (UnsupportedJwtException e) {
+                ResultVO resultVO = new ResultVO(401, "token无效", null);
+                doResponse(response, resultVO);
+                return false;
+            }catch (Exception e){
+                ResultVO resultVO = new ResultVO(401, "请先登录3333333", null);
+                doResponse(response, resultVO);
+
+            }
+            }
+        return false;
+
+            }
+
+    private void doResponse(HttpServletResponse response, ResultVO resultVO) throws Exception {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        String s = new ObjectMapper().writeValueAsString(resultVO);
+        printWriter.print(s);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
+
+
