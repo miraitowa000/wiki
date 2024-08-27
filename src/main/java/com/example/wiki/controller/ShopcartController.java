7b51e6371a999f1067db5f15fe2fc899ca16c85b
ranwei <r13228115513@163.com>
2024-08-27 16:25:16 +0800

    解析token

---
+++ src/main/java/com/example/wiki/controller/ShopcartController.java
@@ -0,0 +1,28 @@
+package com.example.wiki.controller;
+
+import com.example.wiki.dto.ResultVO;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/shopcart")
+public class ShopcartController {
+    @GetMapping("/list")
+    public ResultVO list(String token){
+        if(token == null){
+            return new ResultVO(400,"请先登录",null);
+        }else {
+            JwtParser jwtParser = Jwts.parser();
+            jwtParser.setSigningKey("secret");
+            try {
+                jwtParser.parseClaimsJws(token);
+                return new ResultVO(200,"success",null);
+            }catch (Exception e){
+                return new ResultVO(400,"token无效",null);
+            }
+        }
+    }
+}
